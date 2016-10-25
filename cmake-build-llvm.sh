0315fa4c54544461d2610d8572edfc04d19676e3
beunprepared <beunprepared@gmail.com>
2016-10-25 04:33:24 -0400

    upate cmake-build-llvm.sh

--- cmake-build-llvm.sh
+++ cmake-build-llvm.sh
@@ -1,7 +1,13 @@
 echo "cmake config ..."
-WORKING_PATH=$HOME/working
-cmake -DCMAKE_INSTALL_PREFIX=$WORKING_PATH/install-llvm \
--DPOLLY_ENABLE_GPGPU_CODEGEN=ON \
--DLLVM_ENABLE_LIBCXX=ON \
--G "Unix Makefiles" \
-$WORKING_PATH/llvm;
+if ! [ "$WORKING_PATH" = "" ] ; then
+
+	cmake -DCMAKE_INSTALL_PREFIX=$WORKING_PATH/install-llvm \
+	-DPOLLY_ENABLE_GPGPU_CODEGEN=ON \
+	-DLLVM_ENABLE_LIBCXX=ON \
+	-G "Unix Makefiles" \
+	$WORKING_PATH/llvm;
+
+else
+    echo "export WORKING_PATH first."
+
+fi;
--- dynamic-switch-llvm-ykt.sh
+++ dynamic-switch-llvm-ykt.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
-if [ ! "$WORKING_PATH" = "" ] ; then
+if ! [ "$WORKING_PATH" = "" ] ; then
 	ORIGINAL_SRC_ROOT=$WORKING_PATH/llvm
+	ORIGINAL_BUILD_ROOT=$WORKING_PATH/build-llvm
 	ORIGINAL_OBJ_ROOT=$WORKING_PATH/install-llvm
 
 	SRC_ROOT=$WORKING_PATH/llvm-ykt
+	BUILD_ROOT=$WORKING_PATH/build-llvm-ykt
 	OBJ_ROOT=$WORKING_PATH/install-llvm-ykt
 
 	export SRC_ROOT
 	export OBJ_ROOT
 
 
-	substr="$ORIGINAL_OBJ_ROOT" # "$OBJ_ROOT/usr/local"
-	replace="$OBJ_ROOT"
+	original_build_root="$ORIGINAL_BUILD_ROOT"
+	original_obj_root="$ORIGINAL_OBJ_ROOT"
+	build_root="$BUILD_ROOT"
+	obj_root="$OBJ_ROOT"
+
 	# http://stackoverflow.com/questions/918886/how-do-i-split-a-string-on-a-delimiter-in-bash
 
 	# deal with CPATH
@@ -19,12 +24,19 @@ if [ ! "$WORKING_PATH" = "" ] ; then
 	IFS=':' read -ra ADDR <<< "$CPATH"
 	#while IFS=':' read -ra ADDR; do
 	for i in "${ADDR[@]}"; do
-		if [[ ! ( "${i/$substr}" = "$i" ) ]]; then
+		if [[ ! ( "${i/$original_obj_root}" = "$i" ) ]]; then
 			CPATH=${CPATH/$i}; 
 		fi;
-		if [[ ! ( "${i/$replace}" = "$i" ) ]]; then
+		if [[ ! ( "${i/$obj_root}" = "$i" ) ]]; then
 			CPATH=${CPATH/$i}; 
 		fi;
+		if [[ ! ( "${i/$original_build_root}" = "$i" ) ]]; then
+			CPATH=${CPATH/$i}; 
+		fi;
+		if [[ ! ( "${i/$build_root}" = "$i" ) ]]; then
+			CPATH=${CPATH/$i}; 
+		fi;
+
 	done;
 	#done <<< "$CPATH";
 	ADDR=""
@@ -42,10 +54,10 @@ if [ ! "$WORKING_PATH" = "" ] ; then
 	done
 	#done <<< "$CPATH";
 	if [ "$tmp" = "" ] ; then
-		CPATH=$OBJ_ROOT/include:$OBJ_ROOT/lib:$OBJ_ROOT/bin
+		CPATH=$OBJ_ROOT/include:$OBJ_ROOT/lib:$OBJ_ROOT/bin:$ORIGINAL_OBJ_ROOT/include:$ORIGINAL_OBJ_ROOT/lib:$ORIGINAL_OBJ_ROOT/bin
 #		CPATH=$OBJ_ROOT/include:$tmp
 	else
-		CPATH=$OBJ_ROOT/include:$OBJ_ROOT/lib:$OBJ_ROOT/bin:$tmp
+		CPATH=$OBJ_ROOT/include:$OBJ_ROOT/lib:$OBJ_ROOT/bin:$ORIGINAL_OBJ_ROOT/include:$ORIGINAL_OBJ_ROOT/lib:$ORIGINAL_OBJ_ROOT/bin:$tmp
 #		CPATH=$OBJ_ROOT/include:$tmp
 	fi;
 
@@ -55,12 +67,19 @@ if [ ! "$WORKING_PATH" = "" ] ; then
 	ADDR=""
 	IFS=':' read -ra ADDR <<< "$PATH"
 	for i in "${ADDR[@]}"; do
-		if [[ ! ( "${i/$substr}" = "$i" ) ]]; then
+		if [[ ! ( "${i/$original_obj_root}" = "$i" ) ]]; then
 			PATH=${PATH/$i}; 
 		fi;
-		if [[ ! ( "${i/$replace}" = "$i" ) ]]; then
+		if [[ ! ( "${i/$obj_root}" = "$i" ) ]]; then
 			PATH=${PATH/$i}; 
 		fi;
+		if [[ ! ( "${i/$original_build_root}" = "$i" ) ]]; then
+			PATH=${PATH/$i}; 
+		fi;
+		if [[ ! ( "${i/$build_root}" = "$i" ) ]]; then
+			PATH=${PATH/$i}; 
+		fi;
+
 	done;
 	ADDR=""
 	tmp=""
@@ -75,9 +94,9 @@ if [ ! "$WORKING_PATH" = "" ] ; then
 		fi;
 	done
 	if [ "$tmp" = "" ] ; then
-		PATH=$OBJ_ROOT/include:$OBJ_ROOT/lib:$OBJ_ROOT/bin
+		PATH=$OBJ_ROOT/include:$OBJ_ROOT/lib:$OBJ_ROOT/bin:$ORIGINAL_OBJ_ROOT/include:$ORIGINAL_OBJ_ROOT/lib:$ORIGINAL_OBJ_ROOT/bin
 	else
-		PATH=$OBJ_ROOT/include:$OBJ_ROOT/lib:$OBJ_ROOT/bin:$tmp
+		PATH=$OBJ_ROOT/include:$OBJ_ROOT/lib:$OBJ_ROOT/bin:$ORIGINAL_OBJ_ROOT/include:$ORIGINAL_OBJ_ROOT/lib:$ORIGINAL_OBJ_ROOT/bin:$tmp
 	fi;
 
 	export PATH
@@ -88,12 +107,19 @@ if [ ! "$WORKING_PATH" = "" ] ; then
 	ADDR=""
 	IFS=':' read -ra ADDR <<< "$LD_LIBRARY_PATH"
 	for i in "${ADDR[@]}"; do
-		if [[ ! ( "${i/$substr}" = "$i" ) ]]; then
+		if [[ ! ( "${i/$original_obj_root}" = "$i" ) ]]; then
 			LD_LIBRARY_PATH=${LD_LIBRARY_PATH/$i}; 
 		fi;
-		if [[ ! ( "${i/$replace}" = "$i" ) ]]; then
+		if [[ ! ( "${i/$obj_root}" = "$i" ) ]]; then
 			LD_LIBRARY_PATH=${LD_LIBRARY_PATH/$i}; 
 		fi;
+		if [[ ! ( "${i/$original_build_root}" = "$i" ) ]]; then
+			LD_LIBRARY_PATH=${LD_LIBRARY_PATH/$i}; 
+		fi;
+		if [[ ! ( "${i/$build_root}" = "$i" ) ]]; then
+			LD_LIBRARY_PATH=${LD_LIBRARY_PATH/$i}; 
+		fi;
+
 		if [[ ! ( "${i/$cuda_device}" = "$i" ) ]]; then
 			LD_LIBRARY_PATH=${LD_LIBRARY_PATH/$i}; 
 		fi;
@@ -111,9 +137,9 @@ if [ ! "$WORKING_PATH" = "" ] ; then
 		fi;
 	done
 	if [ "$tmp" = "" ] ; then
-		LD_LIBRARY_PATH=$OBJ_ROOT/lib:$cuda_device
+		LD_LIBRARY_PATH=$OBJ_ROOT/lib:$ORIGINAL_OBJ_ROOT/lib:$cuda_device
 	else
-		LD_LIBRARY_PATH=$OBJ_ROOT/lib:$cuda_device:$tmp
+		LD_LIBRARY_PATH=$OBJ_ROOT/lib:$ORIGINAL_OBJ_ROOT/lib:$cuda_device:$tmp
 	fi;
 	
 	export LD_LIBRARY_PATH
@@ -122,12 +148,19 @@ if [ ! "$WORKING_PATH" = "" ] ; then
 	ADDR=""
 	IFS=':' read -ra ADDR <<< "$LIBRARY_PATH"
 	for i in "${ADDR[@]}"; do
-		if ! [ "${i/$substr}" = "$i" ]; then
+		if ! [ "${i/$original_obj_root}" = "$i" ]; then
 			LIBRARY_PATH=${LIBRARY_PATH/$i}; 
 		fi;
-		if ! [ "${i/$replace}" = "$i" ]; then
+		if ! [ "${i/$obj_root}" = "$i" ]; then
 			LIBRARY_PATH=${LIBRARY_PATH/$i}; 
 		fi;
+		if ! [ "${i/$original_build_root}" = "$i" ]; then
+			LIBRARY_PATH=${LIBRARY_PATH/$i}; 
+		fi;
+		if ! [ "${i/$build_root}" = "$i" ]; then
+			LIBRARY_PATH=${LIBRARY_PATH/$i}; 
+		fi;
+
 		if ! [ "${i##$cuda_device}" = "$i" ]; then
 			LIBRARY_PATH=${LIBRARY_PATH/$i}; 
 		fi;
@@ -145,9 +178,9 @@ if [ ! "$WORKING_PATH" = "" ] ; then
 		fi;
 	done
 	if [ "$tmp" = "" ] ; then
-		LIBRARY_PATH=$OBJ_ROOT/lib:$cuda_device
+		LIBRARY_PATH=$OBJ_ROOT/lib:$ORIGINAL_OBJ_ROOT/lib:$cuda_device
 	else
-		LIBRARY_PATH=$OBJ_ROOT/lib:$cuda_device:$tmp
+		LIBRARY_PATH=$OBJ_ROOT/lib:$ORIGINAL_OBJ_ROOT/lib:$cuda_device:$tmp
 	fi;
 	
 	export LIBRARY_PATH

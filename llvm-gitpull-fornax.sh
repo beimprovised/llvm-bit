90a2b9bcf783ca8d9c992960e50e892f0de31141
Hui Yang <hyang@fornax.ec.oakland.edu>
2016-10-26 08:51:55 -0400

    upate llvm-gitpull-fornax.sh

--- llvm-gitpull-fornax.sh
+++ llvm-gitpull-fornax.sh
@@ -36,8 +36,34 @@ git pull --rebase
 
 cd ../..
 
+echo "------- libcxx ----------"
+cd $WORKING_PATH/llvm/projects/libcxx
+
+git pull --rebase
+
+
 echo "------- libcxxabi ----------"
 
 cd projects/libcxxabi
 
 git pull --rebase
+
+cd $WORKING_PATH/llvm/projects/test-suite
+
+git pull --rebase
+
+echo "------- clang tools extra ----------"
+cd $WORKING_PATH/llvm/tools/clang/tools/clang-tools-extra
+
+git pull --rebase
+
+echo "prepare build directory ..."
+cd "$WORKING_PATH"
+if [ ! -d "build-llvm" ]; then
+        mkdir build-llvm #(in-tree build is not supported)
+fi;
+if [ ! -d "install-llvm" ]; then
+        mkdir install-llvm
+fi;
+cd build-llvm;
+

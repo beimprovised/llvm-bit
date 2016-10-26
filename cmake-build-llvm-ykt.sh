e1f7c187805f66cd670fb399a205b4e30dc03691
beunprepared <beunprepared@gmail.com>
2016-10-26 09:46:12 -0400

    add cmake-install-llvm(-ykt).sh

--- cmake-build-llvm-ykt.sh
+++ cmake-build-llvm-ykt.sh
@@ -3,16 +3,18 @@ if [ -d "$WORKING_PATH" ]; then
 	if [ -d "$TARGET_DIR" ]; then
 	  # Control will enter here if $DIRECTORY exists.
 		cd $TARGET_DIR
-		cmake -DCMAKE_OSX_ARCHITECTURES=x86_64 \
+
+#		cmake -DCMAKE_INSTALL_PREFIX=$WORKING_PATH/install-llvm-ykt -P cmake_install.cmake \
+		cmake -DCMAKE_INSTALL_PREFIX=$WORKING_PATH/install-llvm-ykt \
+		-DCMAKE_OSX_ARCHITECTURES=x86_64 \
 		-DCMAKE_ASM_COMPILER=$(which cc) \
 		-DCMAKE_C_FLAGS="-DOPENMP_NVPTX_COMPUTE_CAPABILITY=37" \
 		-DLIBOMP_CFLAGS="-stdlib=libc++" \
 		-DCMAKE_CXX_FLAGS="-std=c++11 -stdlib=libc++ -DOPENMP_NVPTX_COMPUTE_CAPABILITY=37" \
 		-DCMAKE_BUILD_TYPE=DEBUG \
-		-DCMAKE_INSTALL_PREFIX=$WORKING_PATH/install-llvm-ykt -P cmake_install.cmake \
 		-DCMAKE_CXX_STANDARD_REQUIRED=ON \
-		-DCMAKE_C_COMPILER=$OBJ_ROOT/bin/clang \
-		-DCMAKE_CXX_COMPILER=$OBJ_ROOT/bin/clang++ \
+		-DCMAKE_C_COMPILER=$ORIGINAL_OBJ_ROOT/bin/clang \
+		-DCMAKE_CXX_COMPILER=$ORIGINAL_OBJ_ROOT/bin/clang++ \
 		-DCMAKE_MAKE_PROGRAM=$(which make) \
 		-DCMAKE_CXX_EXTENSIONS=ON \
 		-DCMAKE_CXX_COMPILER_FLAGS="-L -std=c++11" \
@@ -24,7 +26,7 @@ if [ -d "$WORKING_PATH" ]; then
 		-DLIBOMPTARGET_NVPTX_COMPUTE_CAPABILITY=37 \
 		-DCHECK_CXX_SOURCE_COMPILES=ON \
 		-G "Unix Makefiles" \
-		$HOME/working/llvm-ykt
+		$WORKING_PATH/llvm-ykt
 	else
 		echo "can't find the directory \$TARGET_DIR"
 	fi;
--- cmake-install-llvm.sh
+++ cmake-install-llvm.sh
@@ -1,1 +1,18 @@
-cmake -DCMAKE_INSTALL_PREFIX=$WORKING_PATH/install-llvm -P cmake_install.cmake
+echo "cmake config ..."
+if ! [ "$WORKING_PATH" = "" ] ; then
+
+        TARGET_DIR=$WORKING_PATH/build-llvm
+        if [ -d "$TARGET_DIR" ]; then
+          # Control will enter here if $DIRECTORY exists.
+                cd $TARGET_DIR
+
+		cmake -DCMAKE_INSTALL_PREFIX=$WORKING_PATH/install-llvm -P cmake_install.cmake
+        else
+                echo "can't find the directory \$TARGET_DIR"
+        fi;
+
+else
+    echo "export WORKING_PATH first."
+
+fi;
+

08dc891d9d06f5c4fb5c5e3c2ce864a112ab5ee8
beunprepared <beunprepared@gmail.com>
2016-11-08 07:25:10 -0500

    update cmake-build-llvm-official.sh

--- cmake-build-llvm-official.sh
+++ cmake-build-llvm-official.sh
@@ -4,12 +4,12 @@ if ! [ "$WORKING_PATH" = "" ] ; then
         cmake -DCMAKE_INSTALL_PREFIX=$WORKING_PATH/install-llvm \
         -DPOLLY_ENABLE_GPGPU_CODEGEN=ON \
         -DLLVM_ENABLE_LIBCXX=ON \
-        -DCMAKE_C_COMPILER=$ORIGINAL_OBJ_ROOT/bin/clang \
-        -DCMAKE_CXX_COMPILER=$ORIGINAL_OBJ_ROOT/bin/clang++ \
+        -DCMAKE_C_COMPILER=$(which clang) \
+        -DCMAKE_CXX_COMPILER=$(which clang++) \
         -DCMAKE_SHARED_LIBRARY_LINK_C_FLAGS="" \
         -DCMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS="" \
-        -DCMAKE_CXX_FLAGS="-std=c++11" \ # fatal error: 'atomic' file not found
-        -DLIBOMP_CFLAGS="-stdlib=libc++" \ # fatal error: 'atomic' file not found
+        -DCMAKE_CXX_FLAGS="-std=c++11" \
+        -DLIBOMP_CFLAGS="-stdlib=libc++" \
         -G "Unix Makefiles" \
         $WORKING_PATH/llvm;
 

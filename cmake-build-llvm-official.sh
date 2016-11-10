d64f3d5fc2dc7bb05ac1f95ce6893e409e38abef
beunprepared <beunprepared@gmail.com>
2016-11-10 04:10:16 -0500

    update cmake variable type

--- cmake-build-llvm-official.sh
+++ cmake-build-llvm-official.sh
@@ -1,17 +1,17 @@
 echo "cmake config ..."
 if ! [ "$WORKING_PATH" = "" ] ; then
 
-        cmake -DCMAKE_INSTALL_PREFIX=$WORKING_PATH/install-llvm \
-        -DPOLLY_ENABLE_GPGPU_CODEGEN=ON \
-        -DLLVM_ENABLE_LIBCXX=ON \
-        -DCMAKE_C_COMPILER=$(which clang) \
-        -DCMAKE_CXX_COMPILER=$(which clang++) \
-        -DCMAKE_SHARED_LIBRARY_LINK_C_FLAGS="" \
-        -DCMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS="" \
-        -DCMAKE_CXX_FLAGS="-std=c++11" \
-        -DLIBOMP_CFLAGS="-stdlib=libc++" \
+        cmake -DCMAKE_INSTALL_PREFIX:PATH=$WORKING_PATH/install-llvm \
+        -DPOLLY_ENABLE_GPGPU_CODEGEN:BOOL=ON \
+        -DLLVM_ENABLE_LIBCXX:BOOL=ON \
+        -DCMAKE_C_COMPILER:FILEPATH=$(which clang) \
+        -DCMAKE_CXX_COMPILER:FILEPATH=$(which clang++) \
+        -DCMAKE_SHARED_LIBRARY_LINK_C_FLAGS:STRING="" \
+        -DCMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS:STRING="" \
+        -DCMAKE_CXX_FLAGS:STRING="-std=c++11" \
+        -DLIBOMP_CFLAGS:STRING="-stdlib=libc++" \
         -G "Unix Makefiles" \
-        $WORKING_PATH/llvm;
+        "$WORKING_PATH/llvm";
 
 else
     echo "export WORKING_PATH first."

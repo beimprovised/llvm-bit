47866433566c45a7b1cc2a4c6df580d8e576972d
beimprovised <beunprepared@gmail.com>
2016-11-14 07:17:33 -0500

    update cmake-build-llvm-official.sh

--- cmake-build-llvm-official.sh
+++ cmake-build-llvm-official.sh
@@ -4,11 +4,12 @@ if ! [ "$WORKING_PATH" = "" ] ; then
         cmake -DCMAKE_INSTALL_PREFIX:PATH=$WORKING_PATH/install-llvm \
         -DPOLLY_ENABLE_GPGPU_CODEGEN:BOOL=ON \
         -DLLVM_ENABLE_LIBCXX:BOOL=ON \
+	-DLIBOMP_CFLAGS:STRING="-stdlib=libc++" \
+	-DCMAKE_CXX_FLAGS:STRING="-std=c++11 -stdlib=libc++" \
         -DCMAKE_C_COMPILER:FILEPATH=$(which clang) \
         -DCMAKE_CXX_COMPILER:FILEPATH=$(which clang++) \
         -DCMAKE_SHARED_LIBRARY_LINK_C_FLAGS:STRING="" \
         -DCMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS:STRING="" \
-        -DCMAKE_CXX_FLAGS:STRING="-std=c++11" \
         -DLIBOMP_CFLAGS:STRING="-stdlib=libc++" \
         -G "Unix Makefiles" \
         "$WORKING_PATH/llvm";

f78fc175d0c360d7560275d8ba61e380f38b2734
beimprovised <beunprepared@gmail.com>
2016-11-19 07:05:40 -0500

    update dynamic-switch-llvm.sh

--- dynamic-switch-llvm.sh
+++ dynamic-switch-llvm.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
-RELATIVE_SOURCE_PATH_ORIGINAL=$1	# llvm
-RELATIVE_SOURCE_PATH=$2			# llvm-bit or llvm-ykt
-if ! ( [ "$RELATIVE_SOURCE_PATH_ORIGINAL" = "" ] || [ "$RELATIVE_SOURCE_PATH" = "" ] ); then
+ORIGINAL_RELATIVE_INSTALL_PATH=$1	# llvm
+NEW_ABSOLUTE_INSTALL_PATH=$2			# llvm-bit or llvm-ykt
+if ! ( [ "$ORIGINAL_RELATIVE_INSTALL_PATH" = "" ] || [ "$NEW_ABSOLUTE_INSTALL_PATH" = "" ] ); then
 if ! [ "$WORKING_PATH" = "" ] ; then
-	SRC_ROOT_ORIGINAL="$WORKING_PATH/$RELATIVE_SOURCE_PATH_ORIGINAL"
-	BUILD_ROOT_ORIGINAL="$WORKING_PATH/build-$RELATIVE_SOURCE_PATH_ORIGINAL"
-	OBJ_ROOT_ORIGINAL="$WORKING_PATH/install-$RELATIVE_SOURCE_PATH_ORIGINAL"
+#	SRC_ROOT_ORIGINAL="$WORKING_PATH/$ORIGINAL_RELATIVE_INSTALL_PATH"
+#	BUILD_ROOT_ORIGINAL="$WORKING_PATH/build-$ORIGINAL_RELATIVE_INSTALL_PATH"
+#	OBJ_ROOT_ORIGINAL="$WORKING_PATH/install-$ORIGINAL_RELATIVE_INSTALL_PATH"
 
-	SRC_ROOT="$WORKING_PATH/$RELATIVE_SOURCE_PATH"
-	BUILD_ROOT="$WORKING_PATH/build-$RELATIVE_SOURCE_PATH"
-	OBJ_ROOT="$WORKING_PATH/install-$RELATIVE_SOURCE_PATH"
+#	SRC_ROOT="$WORKING_PATH/$NEW_ABSOLUTE_INSTALL_PATH"
+#	BUILD_ROOT="$WORKING_PATH/build-$NEW_ABSOLUTE_INSTALL_PATH"
+	OBJ_ROOT=$NEW_ABSOLUTE_INSTALL_PATH	#"$WORKING_PATH/install-$NEW_ABSOLUTE_INSTALL_PATH"
 
-	export SRC_ROOT
+#	export SRC_ROOT
 	export OBJ_ROOT
 
 	# http://stackoverflow.com/questions/918886/how-do-i-split-a-string-on-a-delimiter-in-bash
@@ -22,7 +22,7 @@ if ! [ "$WORKING_PATH" = "" ] ; then
 	IFS=':' read -ra ADDR <<< "$CPATH"
 	#while IFS=':' read -ra ADDR; do
 	for i in "${ADDR[@]}"; do
-		if [[ ( "${i//$RELATIVE_SOURCE_PATH_ORIGINAL}" = "$i" ) && ( "${i//$RELATIVE_SOURCE_PATH}" = "$i" ) ]]; then
+		if [[ ( "${i//$ORIGINAL_RELATIVE_INSTALL_PATH}" = "$i" ) && ( "${i//$NEW_ABSOLUTE_INSTALL_PATH}" = "$i" ) ]]; then
 			if [ "$tmp" = "" ] ; then
 				tmp=$i;
 #				echo $i;
@@ -51,7 +51,7 @@ if ! [ "$WORKING_PATH" = "" ] ; then
 	IFS=':' read -ra ADDR <<< "$PATH"
 	#while IFS=':' read -ra ADDR; do
 	for i in "${ADDR[@]}"; do
-		if [[ ( "${i//$RELATIVE_SOURCE_PATH_ORIGINAL}" = "$i" ) && ( "${i//$RELATIVE_SOURCE_PATH}" = "$i" ) ]]; then
+		if [[ ( "${i//$ORIGINAL_RELATIVE_INSTALL_PATH}" = "$i" ) && ( "${i//$NEW_ABSOLUTE_INSTALL_PATH}" = "$i" ) ]]; then
 			if [ "$tmp" = "" ] ; then
 				tmp=$i;
 #				echo $i;
@@ -83,7 +83,7 @@ if ! [ "$WORKING_PATH" = "" ] ; then
 		tmp=""
 		IFS=':' read -ra ADDR <<< "$LD_LIBRARY_PATH"
 		for i in "${ADDR[@]}"; do
-			if [[ ( "${i//$RELATIVE_SOURCE_PATH_ORIGINAL}" = "$i" ) && ( "${i//$RELATIVE_SOURCE_PATH}" = "$i" ) && ( "${i//$ACCELERATOR_TOOLKIT}" = "$i" ) ]]; then
+			if [[ ( "${i//$ORIGINAL_RELATIVE_INSTALL_PATH}" = "$i" ) && ( "${i//$NEW_ABSOLUTE_INSTALL_PATH}" = "$i" ) && ( "${i//$ACCELERATOR_TOOLKIT}" = "$i" ) ]]; then
 				if [ "$tmp" = "" ] ; then
 					tmp=$i;
 #					echo $i;
@@ -110,7 +110,7 @@ if ! [ "$WORKING_PATH" = "" ] ; then
 		tmp=""
 		IFS=':' read -ra ADDR <<< "$LIBRARY_PATH"
 		for i in "${ADDR[@]}"; do
-			if [[ ( "${i//$RELATIVE_SOURCE_PATH_ORIGINAL}" = "$i" ) && ( "${i//$RELATIVE_SOURCE_PATH}" = "$i" ) && ( "${i//$ACCELERATOR_TOOLKIT}" = "$i" ) ]]; then
+			if [[ ( "${i//$ORIGINAL_RELATIVE_INSTALL_PATH}" = "$i" ) && ( "${i//$NEW_ABSOLUTE_INSTALL_PATH}" = "$i" ) && ( "${i//$ACCELERATOR_TOOLKIT}" = "$i" ) ]]; then
 				if [ "$tmp" = "" ] ; then
 					tmp=$i;
 #					echo $i;
@@ -138,10 +138,10 @@ else
 	echo "export WORKING_PATH first."
 fi;
 else
-	echo "Provide \$RELATIVE_SOURCE_PATH_ORIGINAL and \$RELATIVE_SOURCE_PATH from arguments 1 and 2."
+	echo "Provide \$ORIGINAL_RELATIVE_INSTALL_PATH and \$NEW_ABSOLUTE_INSTALL_PATH from arguments 1 and 2."
 fi;
-#export OMPTARGET_LIBS=/home/hyang/install-$RELATIVE_SOURCE_PATH/lib	#<path to libomptarget project>/lib
-#export LIBOMP_LIB=/home/hyang/install-$RELATIVE_SOURCE_PATH/lib	#<path to libomp project>/runtime/p8build/src/
+#export OMPTARGET_LIBS=/home/hyang/install-$NEW_ABSOLUTE_INSTALL_PATH/lib	#<path to libomptarget project>/lib
+#export LIBOMP_LIB=/home/hyang/install-$NEW_ABSOLUTE_INSTALL_PATH/lib	#<path to libomp project>/runtime/p8build/src/
 #export LIBRARY_PATH=$OMPTARGET_LIBS:$LIBRARY_PATH
 #export LD_LIBRARY_PATH=$LIBOMP_LIB:$OMPTARGET_LIBS:/opt/nvidia/cuda/lib64:$LD_LIBRARY_PATH
 

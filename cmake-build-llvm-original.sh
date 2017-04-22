334cc7a1200b501c268adaeeb4d14d92fd199986
beimprovised <beunprepared@gmail.com>
2017-04-22 07:54:10 -0400

    update cmake-build-llvm-original.sh

--- cmake-build-llvm-original.sh
+++ cmake-build-llvm-original.sh
@@ -1,28 +1,41 @@
+#!/bin/bash
 echo "cmake config ..."
+#inf_file=$1
+#if [ -f $inf_file ] ; then
+#    . $inf_file
+#else
+#	echo "Provide correct .inf would be appreciated." 
+#fi
 
+#for file in build-llvm-dev.inf; do
+#    echo "Running on: $file"
+#    while IFS='|' read -r CMAKE RELATIVE_SOURCE_PATH BUILD_PATH_PREFIX INSTALL_PATH_PREFIX COMPILER_SWITCH CLANG_MAIN_VERSION ATOMIC_PATH MAKE_PROGRAM PROJECT_TYPE
+#    do
 
-#PYTHON_PATH=$(which python)//"/bin/python"
-#	-DPYTHON_EXECUTABLE:FILEPATH=$(which python) \
-#	-DPYTHON_INCLUDE_DIR:PATH=$PYTHON_PATH \
-#	-DPYTHON_LIBRARY:FILEPATH=$PYTHON_PATH/lib64 \
-#	-DPYTHON_LIBRARY_DEBUG:FILEPATH=PYTHON_LIBRARY_DEBUG-NOTFOUND \
+##PYTHON_PATH=$(which python)//"/bin/python"
+##	-DPYTHON_EXECUTABLE:FILEPATH=$(which python) \
+##	-DPYTHON_INCLUDE_DIR:PATH=$PYTHON_PATH \
+##	-DPYTHON_LIBRARY:FILEPATH=$PYTHON_PATH/lib64 \
+##	-DPYTHON_LIBRARY_DEBUG:FILEPATH=PYTHON_LIBRARY_DEBUG-NOTFOUND \
 
-CMAKE=$1
-SOURCE_PATH=$2
-BUILD_PATH_PREFIX=$3
-INSTALL_PATH_PREFIX=$4
-COMPILER_SWITCH=$5
-CLANG_MAIN_VERSION=$6
-ATOMIC_PATH=$7
-PROJECT_TYPE=$8
-#DOXYGEN_DOT_EXECUTABLE=$6 	#/usr/bin/dot
-#DOXYGEN_EXECUTABLE=$7 		#/usr/bin/doxygen
+#CMAKE=$1
+#RELATIVE_SOURCE_PATH=$2
+#BUILD_PATH_PREFIX=$3
+#INSTALL_PATH_PREFIX=$4
+#INSTALL_ROOT=$WORKING_PATH/$INSTALL_PATH_PREFIX-$RELATIVE_SOURCE_PATH
+#COMPILER_SWITCH=$5
+#CLANG_MAIN_VERSION=$6
+#ATOMIC_PATH=$7
+#MAKE_PROGRAM=$8
+#PROJECT_TYPE=$9
+##DOXYGEN_DOT_EXECUTABLE=$6 	#/usr/bin/dot
+##DOXYGEN_EXECUTABLE=$7 		#/usr/bin/doxygen
 
 if ! [ "$WORKING_PATH" = "" ] ; then
 
 	if ! [ "$CMAKE" = "" ] ; then
 
-		if ! [ "$SOURCE_PATH" = "" ] ; then
+		if ! [ "$RELATIVE_SOURCE_PATH" = "" ] ; then
 
 			if ! [ "$BUILD_PATH_PREFIX" = "" ] ; then
 
@@ -42,25 +55,27 @@ if ! [ "$WORKING_PATH" = "" ] ; then
 						if [[ ( "$CC" != "" ) && ( "$CPP" != "" ) ]]; then
 							if ! [ "$CLANG_MAIN_VERSION" = "" ]; then
 								if ! [ "$ATOMIC_PATH" = "" ]; then
+								if ! [ "$MAKE_PROGRAM" = "" ]; then 
 								if ! [ "$PROJECT_TYPE" = "" ]; then   
 #								if ! [ "$DOXYGEN_DOT_EXECUTABLE" = "" ] ; then
 #
 #									if ! [ "$DOXYGEN_EXECUTABLE" = "" ] ; then
 
-										if ! [ -d "$WORKING_PATH/$BUILD_PATH_PREFIX-$SOURCE_PATH" ]; then
-											mkdir "$WORKING_PATH/$BUILD_PATH_PREFIX-$SOURCE_PATH"
+										if ! [ -d "$WORKING_PATH/$BUILD_PATH_PREFIX-$RELATIVE_SOURCE_PATH" ]; then
+											mkdir "$WORKING_PATH/$BUILD_PATH_PREFIX-$RELATIVE_SOURCE_PATH"
 										fi;
-										if ! [ "$WORKING_PATH/$BUILD_PATH_PREFIX-$SOURCE_PATH" = "$PWD" ]; then
-											cd "$WORKING_PATH/$BUILD_PATH_PREFIX-$SOURCE_PATH"
+										if ! [ "$WORKING_PATH/$BUILD_PATH_PREFIX-$RELATIVE_SOURCE_PATH" = "$PWD" ]; then
+											cd "$WORKING_PATH/$BUILD_PATH_PREFIX-$RELATIVE_SOURCE_PATH"
 										fi;
-										if [ "$PWD" = "$WORKING_PATH/$BUILD_PATH_PREFIX-$SOURCE_PATH" ]; then
-											$CMAKE -DCMAKE_INSTALL_PREFIX:PATH=$WORKING_PATH/$INSTALL_PATH_PREFIX-$SOURCE_PATH \
+										if [ "$PWD" = "$WORKING_PATH/$BUILD_PATH_PREFIX-$RELATIVE_SOURCE_PATH" ]; then
+											$CMAKE -DCMAKE_INSTALL_PREFIX:PATH=$WORKING_PATH/$INSTALL_PATH_PREFIX-$RELATIVE_SOURCE_PATH \
+											-DCMAKE_MAKE_PROGRAM:FILEPATH=$MAKE_PROGRAM \	
 											-DCMAKE_CXX_FLAGS:STRING="-std=c++11 -stdlib=libc++ -I$WORKING_PATH/$INSTALL_PATH_PREFIX-$RELATIVE_SOURCE_PATH/lib/clang/$CLANG_MAIN_VERSION.0.0/include -I$WORKING_PATH/$INSTALL_PATH_PREFIX-$RELATIVE_SOURCE_PATH/include/c++/v1" \
 											-DCMAKE_C_FLAGS:STRING="-I$WORKING_PATH/$INSTALL_PATH_PREFIX-$RELATIVE_SOURCE_PATH/lib/clang/$CLANG_MAIN_VERSION.0.0/include -I$WORKING_PATH/$INSTALL_PATH_PREFIX-$RELATIVE_SOURCE_PATH/include/c++/v1" \
 											-DCMAKE_EXE_LINKER_FLAGS="-L$WORKING_PATH/$INSTALL_PATH_PREFIX-$RELATIVE_SOURCE_PATH/lib -lc++" \
 											-DCMAKE_CXX_LINK_FLAGS:STRING="-L$WORKING_PATH/$INSTALL_PATH_PREFIX-$RELATIVE_SOURCE_PATH/lib -L${ATOMIC_PATH} -Wl,-rpath,$WORKING_PATH/$INSTALL_PATH_PREFIX-$RELATIVE_SOURCE_PATH/lib -lc++ -lc++abi -latomic" \
-											-DCMAKE_C_COMPILER:FILEPATH=$(which $CC) \
-											-DCMAKE_CXX_COMPILER:FILEPATH=$(which $CPP) \
+											-DCMAKE_C_COMPILER:FILEPATH=$INSTALL_ROOT/bin/$CC \
+											-DCMAKE_CXX_COMPILER:FILEPATH=$INSTALL_ROOT/bin/$CPP \
 										`#	-DLLDB_DISABLE_LIBEDIT:BOOL=ON` \
 											-DCMAKE_ASM_COMPILER_WORKS:INTERNAL=1 \
 											-DLIBCXX_HAVE_CXX_ATOMICS_WITHOUT_LIB:INTERNAL=1 \
@@ -77,7 +92,7 @@ if ! [ "$WORKING_PATH" = "" ] ; then
 										`#	-DLLDB_EXPORT_ALL_SYMBOLS:BOOL=ON` \
 											-DCMAKE_BUILD_TYPE:STRING="Debug" \
 											-G "$PROJECT_TYPE" \
-											"$WORKING_PATH/$SOURCE_PATH";
+											"$WORKING_PATH/$RELATIVE_SOURCE_PATH";
 										else
 											echo "Have not entered destination. Operation failed."
 										fi;
@@ -89,9 +104,12 @@ if ! [ "$WORKING_PATH" = "" ] ; then
 #									echo "Provide \$DOXYGEN_DOT_EXECUTABLE (just dot, not doxygen itself) from argument 6, please."
 #								fi;
 								else                                                        
-									echo "Provide \$PROJECT_TYPE from argument 8."      
+									echo "Provide \$PROJECT_TYPE from argument 9."      
 								fi;                                                        
- 
+ 								else                                                        
+									echo "Provide \$MAKE_PROGRAM from argument 8."      
+								fi;                                                        
+
 								else
 									echo "Provide \$ATOMIC_PATH from argument 7."
 								fi;
@@ -112,7 +130,7 @@ if ! [ "$WORKING_PATH" = "" ] ; then
 				echo "Provide \$BUILD_PATH_PREFIX from argument 3 (e.g.: \"build\"), please."
 			fi;
 		else
-		    echo "Provide \$SOURCE_PATH from argument 2 (e.g.: \"llvm\"), please."
+		    echo "Provide \$RELATIVE_SOURCE_PATH from argument 2 (e.g.: \"llvm\"), please."
 
 		fi;
 	else
@@ -123,4 +141,7 @@ else
     echo "Export \$WORKING_PATH first."
 
 fi;
+#    done < "$file"
+#done
+
 
